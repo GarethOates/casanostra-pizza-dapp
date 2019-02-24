63630c26bd4a638ecaca8aa697dd13e4102bb720
Gareth Oates <gareth@enso.no>
2019-02-24 02:03:17 +0100

    Alignment of pizzas fixed

--- src/layouts/home/PizzaBox.jsx
+++ src/layouts/home/PizzaBox.jsx
@@ -3,30 +3,27 @@ import styled from 'styled-components'
 
 const StyledPizzaBox = styled.div`
     display: inline-flex;
-    flex-direction: row;
+    flex-direction: column;
     flex-wrap: wrap;
     margin: 1rem;
-    width: 25%;
-    align-items: stretch;
+    width: 15%;
+    align-items: center;
     justify-content: stretch;
     background-color: white;
     border: solid 1px rgba(0,0,0,0.56);
 
     & img {
+        display: block;
         height: 5rem;
         margin: 1rem;
-    }
-
-    .name {
-        display: inline;
     }
 `
 const pizzaBox = ({ image, name, description }) => {
     return (
         <StyledPizzaBox>
-            <img src={image} alt="pizza" />
-            <div class="name">{name}</div>
-            <div>{description}</div>
+            <img src={image} alt="pizza" /><br/>
+            <p>{name}</p>
+            <p>{description}</p>
         </StyledPizzaBox>
     )
 }
